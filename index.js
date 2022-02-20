f5096bbed8b53d86a5a8899a578a60a33e6c9db5
kajxqeirscl <laianhalom25@gmail.com>
2022-02-20 17:58:52 +0300

    initialize github repository

---
+++ index.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js")
+require("dotenv").config()
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+})
+
+client.on("ready", () => {
+    console.log(`logged in as ${client.user.tag}`)
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "ping"){
+        message.reply("hello")
+    }
+})
+
+client.login(process.env.TOKEN)
